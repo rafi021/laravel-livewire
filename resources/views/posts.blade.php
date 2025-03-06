07f599bf984a1373bf10b4132e8d873cb71285e5
rafi021 <mahmud.ibrahim021@gmail.com>
2025-03-06 23:38:18 +0600

    create post route and blade file

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,8 @@
+<x-layouts.app title="Dashboard">
+    <div class="relative mb-6 w-full">
+        <flux:heading size="xl" level="1">{{ __('Posts') }}</flux:heading>
+        <flux:subheading size="lg" class="mb-6">{{ __('Manage your all the posts') }}
+        </flux:subheading>
+        <flux:separator variant="subtle" />
+    </div>
+</x-layouts.app>
